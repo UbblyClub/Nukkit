d50e463107a69b07c76d8a39eadc8073cc0d2d90
Denzel Code <denzelcodedev@gmail.com>
2020-11-24 04:20:46 -0500

    fix: human instance fixed

--- src/main/java/cn/nukkit/entity/EntityHuman.java
+++ src/main/java/cn/nukkit/entity/EntityHuman.java
@@ -196,8 +196,12 @@ public class EntityHuman extends EntityHumanType {
                 this.setSkin(newSkin);
             }
 
-            this.uuid = Utils.dataToUUID(String.valueOf(this.getId()).getBytes(StandardCharsets.UTF_8), this.getSkin()
-                    .getSkinData().data, this.getNameTag().getBytes(StandardCharsets.UTF_8));
+            if (skin != null) {
+                this.uuid = Utils.dataToUUID(String.valueOf(this.getId()).getBytes(StandardCharsets.UTF_8), this.getSkin()
+                        .getSkinData().data, this.getNameTag().getBytes(StandardCharsets.UTF_8));
+            } else {
+                this.uuid = Utils.dataToUUID(new byte[][]{String.valueOf(this.getId()).getBytes(StandardCharsets.UTF_8), this.getNameTag().getBytes(StandardCharsets.UTF_8)});
+            }
         }
 
         super.initEntity();
