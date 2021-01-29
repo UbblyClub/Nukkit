65c4e7a6f7f5297c3f7614c2b39e141b99758080
Denzel Code <denzelcodedev@gmail.com>
2021-01-29 13:14:02 -0500

    feat: api

--- src/main/java/cn/nukkit/command/SimpleCommandMap.java
+++ src/main/java/cn/nukkit/command/SimpleCommandMap.java
@@ -212,10 +212,10 @@ public class SimpleCommandMap implements CommandMap {
         int start = 0;
 
         for (int i = 0; i < sb.length(); i++) {
-//            if (sb.charAt(i) == '\\') {
-//                sb.deleteCharAt(i);
-//                continue;
-//            }
+            if (i != sb.length() - 1 && sb.charAt(i) == '\\' && sb.charAt(i + 1) == ' ') {
+                sb.deleteCharAt(i);
+                continue;
+            }
 
             if (sb.charAt(i) == ' ' && notQuoted) {
                 String arg = sb.substring(start, i);
